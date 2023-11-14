1e660d762f63dce6bf482e1c3708539a903ccd65
Hector Matus <{ID}+{username}@users.noreply.github.com>
2023-11-14 09:42:57 -0600

    Added grid card list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter, DM_Sans } from 'next/font/google'
-import './globals.css'
+import './globals.scss'
 
 const dmSans = DM_Sans({ subsets: ['latin'] })
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import styles from './page.module.css';
-export default function Home() {
+import React from "react";
+import styles from "./page.module.scss";
+import { CardI } from "@/models/Card.model";
+import Card from "@/components/elements/Card/Card";
+import Image from "next/image";
+async function getData() {
+  try {
+    const res = await fetch(
+      "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json"
+    );
+    if (!res.ok) {
+      throw new Error(`${res}`);
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("🚀 ~ file: page.tsx:11 ~ getData ~ error:", error);
+  }
+}
+export default async function Home() {
+  const data = await getData();
+  console.log("🚀 ~ file: page.tsx:19 ~ Home ~ data:", data);
   return (
     <main>
       <div className={styles.container}>
-        
+        <img className={styles.img} src={"./images/bg-cafe.jpg"} />
+        <div className={styles.overlay}>
+          <div className={styles["header-container"]}>
+            <h2>Our Collection</h2>
+            <p>
+              Introducing our Coffee Collection, a selection of unique coffees
+              from different roast types and origins, expertly roasted in small
+              batches and shipped fresh weekly.
+            </p>
+          </div>
+          <div className={styles['store-container']}>
+            {data.map((card, index) => (
+              <Card card={card} key={index} />
+            ))}
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
